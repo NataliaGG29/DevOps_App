c5b25e7557f5dd7d06a3a3d5e27da94bef57153b
Natalia Guevara <62606257+NataliaGG29@users.noreply.github.com>
2025-09-09 15:40:51 +1000

    calculatorApp.py

--- calculatorApp.py
+++ calculatorApp.py
@@ -1,14 +1,14 @@
 import tkinter as tk
 
-# Update input
 
+# Update input
 def click_button(item):
     global expression
     expression += str(item)
     input_text.set(expression)
 
-# Evaluate the result
 
+# Evaluate the result
 def evaluate():
     global expression
     try:
@@ -19,14 +19,15 @@ def evaluate():
         input_text.set("Error")
         expression = ""
 
-# clean the input
 
+# Clean the input
 def clear():
     global expression
     expression = ""
     input_text.set("")
 
-# Set up the principal window
+
+# Set up the main window
 window = tk.Tk()
 window.title("Calculator")
 window.geometry("400x500")
@@ -34,12 +35,22 @@ window.geometry("400x500")
 expression = ""
 input_text = tk.StringVar()
 
-# input
+
+# Input frame
 input_frame = tk.Frame(window)
 input_frame.pack(pady=10)
-input_field = tk.Entry(input_frame, textvariable=input_text, font=('arial', 18, 'bold'), width=50, bd=5, insertwidth=4, justify='right')
+input_field = tk.Entry(
+    input_frame,
+    textvariable=input_text,
+    font=('arial', 18, 'bold'),
+    width=50,
+    bd=5,
+    insertwidth=4,
+    justify='right'
+)
 input_field.grid(row=0, column=0)
 input_field.pack(ipady=10)
+
 
 # Buttons
 button_frame = tk.Frame(window)
@@ -53,12 +64,28 @@ buttons = [
 ]
 
 for (text, row, col) in buttons:
-    button = tk.Button(button_frame, text=text, font=('arial', 18, 'bold'), fg='black', width=10, height=3, bd=1,
-                       command=lambda t=text: click_button(t) if t != '=' else evaluate())
+    button = tk.Button(
+        button_frame,
+        text=text,
+        font=('arial', 18, 'bold'),
+        fg='black',
+        width=10,
+        height=3,
+        bd=1,
+        command=lambda t=text: click_button(t) if t != '=' else evaluate()
+    )
     button.grid(row=row, column=col, padx=1, pady=1)
 
-clear_button = tk.Button(button_frame, text='C', font=('arial', 18, 'bold'), fg='black', width=10, height=3, bd=1, command=clear)
+clear_button = tk.Button(
+    button_frame,
+    text='C',
+    font=('arial', 18, 'bold'),
+    fg='black',
+    width=10,
+    height=3,
+    bd=1,
+    command=clear
+)
 clear_button.grid(row=4, column=0, padx=1, pady=1)
 
 window.mainloop()
-
